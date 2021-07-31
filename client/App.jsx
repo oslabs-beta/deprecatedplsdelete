0cc4ea24aac8b21404194aa26231969fb8e2546f
alexr1der <myspacelexr1der@gmail.com>
2021-07-31 11:55:08 -0400

    functionality of front enddone

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
+// import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts'
 
 const dummyData = []
 
@@ -38,12 +38,11 @@ class App extends Component {
   }
 
   render() {
-       const arr = [ 'ALL','AFN','ARS','AWG', 'AUD','AZN','BSD','BBD', 'BDT','BYR','BZD','BMD','BOB', 'BAM','BWP', 'BGN','BRL', 'BND', 'KHR', 'CAD', 'KYD', 'CLP', 'CNY', 'COP', 'CRC','HRK','CUP', 'CZK', 'DKK', 'DOP', 'XCD', 'EGP', 'SVC','EEK', 'EUR','FKP', 'FJD','GHC','GIP','GTQ','GGP','GYD','HNL','HKD','HUF','ISK','INR','IDR','IRR','IMP','ILS','JMD','JPY','JEP','KZT','KPW','KRW','KGS','LAK','LVL', 'LBP','LRD','LTL','MKD','MYR','MUR','MXN','MNT','MZN','NAD','NPR','ANG','NZD','NIO','NGN','NOK','OMR','PKR','PAB','PYG','PEN','PHP','PLN','QAR','RON','RUB','SHP','SAR','RSD','SCR','SGD','SBD','SOS','ZAR','LKR','SEK','CHF','SRD','SYP','TWD','THB','TTD','TRL','TVD','UAH','GBP','USD','UYU','UZS','VEF','VND','YER','ZWD'];
-       const options = arr.map(el => <option > {el} </option>);
+     
     return (
       <>
-        <ConversionBox/>
-        <ChoiceBox/>
+        <ConversionBox info= {this.state} />
+        <ChoiceBox info={this.state} curr1Change={this.curr1Change} curr2Change={this.curr2Change}  valueChange={this.valueChange} />
       </>
     );
   }
@@ -55,6 +54,7 @@ class Toolbar extends Component {
 }
 
 class ConversionBox extends Component {
+
   render() {
     return(
       <>
@@ -64,16 +64,16 @@ class ConversionBox extends Component {
           <div className="amountToConvert">
             {/* amt & curr1 to convert */}
             <label className="">Your amount</label>
-            <div className=""> {this.state.value}</div>
+            <div className=""> {this.props.info.value}</div>
         </div>
         <div className="CurrToConvert">
             {/* amt & curr1 to convert */}
           <label className="">Your input currency</label>
-          <div className=""> {this.state.curr1}</div>
+          <div className=""> {this.props.info.curr1}</div>
         </div>
         <div className="convertedAmount">
           <label className=""></label>
-          <div className=""> {this.state.value * this.state.output} IN {this.state.curr2}</div>
+          <div className=""> {this.props.info.value * this.props.info.output} IN {this.props.info.curr2}</div>
         </div>
         </div>
         
@@ -85,22 +85,24 @@ class ConversionBox extends Component {
 
 class ChoiceBox extends Component {
   render() {
+        const arr = [ 'ALL','AFN','ARS','AWG', 'AUD','AZN','BSD','BBD', 'BDT','BYR','BZD','BMD','BOB', 'BAM','BWP', 'BGN','BRL', 'BND', 'KHR', 'CAD', 'KYD', 'CLP', 'CNY', 'COP', 'CRC','HRK','CUP', 'CZK', 'DKK', 'DOP', 'XCD', 'EGP', 'SVC','EEK', 'EUR','FKP', 'FJD','GHC','GIP','GTQ','GGP','GYD','HNL','HKD','HUF','ISK','INR','IDR','IRR','IMP','ILS','JMD','JPY','JEP','KZT','KPW','KRW','KGS','LAK','LVL', 'LBP','LRD','LTL','MKD','MYR','MUR','MXN','MNT','MZN','NAD','NPR','ANG','NZD','NIO','NGN','NOK','OMR','PKR','PAB','PYG','PEN','PHP','PLN','QAR','RON','RUB','SHP','SAR','RSD','SCR','SGD','SBD','SOS','ZAR','LKR','SEK','CHF','SRD','SYP','TWD','THB','TTD','TRL','TVD','UAH','GBP','USD','UYU','UZS','VEF','VND','YER','ZWD'];
+       const options = arr.map((el, i) => <option key={i}> {el} </option>);
     return(
       <>
         <div className="wrapperChoicebox" >
-         <label className="">Currency 1</label>
-          <select value={this.state.curr1} onChange={this.curr1Change}  required>
-            <option >Choose...</option>
-              {options}
+         <label className="c1test">Currency 1</label>
+          <select value={this.props.info.curr1} onChange={this.props.curr1Change} required>
+            <option>Choose...</option>
+            {options}
           </select>
-          <label  className="">Currency 2</label>
-          <select value={this.state.curr2} onChange={this.curr2Change}  required>
+          <label  className="c2test">Currency 2</label>
+          <select value={this.props.info.curr2} onChange={this.props.curr2Change} required>
             <option >Choose...</option>
             {options}
           </select>
         </div>
         <div className="wrapperInput">
-          <input type="text" value={this.state.value} onChange={this.valueChange} className="inputAmount" placeholder="Input your amout here" required/>
+          <input type="text" value={this.props.info.value} onChange={this.props.valueChange} className="inputAmount" placeholder="Input your amount here" required />
         </div>
       </>
   )}
