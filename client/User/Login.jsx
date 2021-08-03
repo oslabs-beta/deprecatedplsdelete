e7b6132fcffdd556c86d6c563f2ea09ef8bd636b
Alex Zayas <zayas.alexander@gmail.com>
2021-08-03 19:26:11 -0400

    setup react router

--- client/User/Login.jsx
+++ client/User/Login.jsx
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { GoogleLogin } from 'react-google-login';
 
 class Login extends Component {
-  constructor () {
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+  // constructor () {
+  //   this.state = {
+  //     username: '',
+  //     password: '',
+  //     isLoggedIn: false,
+  //     failedLogin: false, 
+  //   };
+  // }
 
   render() {
     return (
+      
       <div>
-        <form method="POST" action="/login">
+        <h1>Here's the login page</h1> 
+          <GoogleLogin
+          clientId=""
+          buttonText="Login with Google"
+          // onSuccess={responseGoogle}
+          // onFailure={responseGoogle}
+          cookiePolicy="single_host_origin"
+        />
+        {/* <form method="POST" action="/login">
           <label> User Name: </label>
           <input type="text" className="username" onChange={(e) => this.setState({...this.state, username: e.target.value})}/>
           <label >Password</label>
           <input type="password" className="password" onChange={(e) => this.setState({...this.state, password: e.target.value})}/>
           <input type="submit" className="Login-btn" text="Login" />
-        </form>
+        </form> */}
       </div>
-    )
+    );
   }
 }
 
--- client/User/Signup.jsx
+++ client/User/Signup.jsx
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class Signup extends Component {
-  constructor () {
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+  // constructor () {
+  //   this.state = {
+  //     username: '',
+  //     password: '',
+  //   };
+  // }
 
   render() {
     return (
       <div>
-        <form method="POST" action="/signup">
-          <label> User Name: </label>
-          <input type="text" className="username" onChange={(e) => this.setState({...this.state, username: e.target.value})}/>
-          <label >Password</label>
-          <input type="password" className="password" onChange={(e) => this.setState({...this.state, password: e.target.value})}/>
-          <input type="submit" className="Signup-btn" text="Signup" />
-        </form>
+        <h1>You made it to signup</h1>
       </div>
-    )
+
+
+      // <div>
+      //   <form method="POST" action="/signup">
+      //     <label> User Name: </label>
+      //     <input type="text" className="username" onChange={(e) => this.setState({...this.state, username: e.target.value})}/>
+      //     <label >Password</label>
+      //     <input type="password" className="password" onChange={(e) => this.setState({...this.state, password: e.target.value})}/>
+      //     <input type="submit" className="Signup-btn" text="Signup" />
+      //   </form>
+      // </div>
+    );
   }
 }
 
