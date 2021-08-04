1dbdccf64770d8be8b2652a24e639738212e82d2
Alex Zayas <zayas.alexander@gmail.com>
2021-08-03 20:06:43 -0400

    started google oAuth

--- client/User/Login.jsx
+++ client/User/Login.jsx
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { GoogleLogin } from 'react-google-login';
 
+
 class Login extends Component {
-  // constructor () {
-  //   this.state = {
-  //     username: '',
-  //     password: '',
-  //     isLoggedIn: false,
-  //     failedLogin: false, 
-  //   };
+  constructor (props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      isLoggedIn: false,
+      failedLogin: false, 
+    };
+    this.responseGoogle = this.responseGoogle.bind(this);
+  }
+
+  // async handleLogin(googleData) {
+  //   const res = await fetch("/api/v1/auth/google", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //   token: googleData.tokenId
+  //     }),
+  //       headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  // const data = await res.json()
+  // // store returned user somehow
   // }
+  responseGoogle(response) {
+    console.log(response);
+  }
+  
 
   render() {
     return (
@@ -18,12 +39,15 @@ class Login extends Component {
       <div>
         <h1>Here's the login page</h1> 
           <GoogleLogin
-          clientId=""
+          clientId="689937676919-hqbq0jspagnb2003k5qp25melhte9t0c.apps.googleusercontent.com"
           buttonText="Login with Google"
-          // onSuccess={responseGoogle}
-          // onFailure={responseGoogle}
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
           cookiePolicy="single_host_origin"
         />
+
+  
+
         {/* <form method="POST" action="/login">
           <label> User Name: </label>
           <input type="text" className="username" onChange={(e) => this.setState({...this.state, username: e.target.value})}/>
