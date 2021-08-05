e07ae0b75cf17487f04ef0adde7333b2f2c8fb80
Brian Grosso <bgro63@gmail.com>
2021-08-05 16:46:55 -0400

    chg base curr working

--- client/App.jsx
+++ client/App.jsx
@@ -8,33 +8,12 @@ import Graph from './Components/Graph.jsx';
 import Navigation from './Components/Navigation.jsx';
 import ConversionBox from './Components/ConversionBox.jsx';
 import PositionsTable from './Components/PositionsTable.jsx'
-// design login/signup page hasan/shawn
-// connect to router
-// fix x&y axis
-// style the page lucas
-
-// After discussion & input from Mike, ISSUE IS:
-// Back end is giving all data at once. Unless they can separate out functionality, state cannot be separated between graph and stateholder and must re-render on every single change.
-// Else, we can BLOW UP state to hold ALL database for the user as well as current conversion rates & amounts. This would slow down the app significantly.
 
 // | App
 //   |Graph (Graph) (only accesses database?)
 //   |StateHolder
 //     |ConversionBox
 //     |ChoiceBox
-
-// hello fellow 27 iterators! I did the frontend more so it might be a little more commented. Feel free to message if any questions!
-// basically what we didn't get to was:
-  // the login and signup pages literally don't render. threw in oleksii.js at the last min, still didn't render. not sure if react router is necessary. issue is same as before all that was added
-  // oauth, user accounts
-  // database management: tracking each person's fav currencies and those over time. holding those in the database. please consider if the conversion to SQL might contribute very handily to these relations
-  // oleksii & i tried for an evening converting to hooks, but scope of data was so small for today that we decided against it. if you expand, please consider!
-  // compare against other investment trends ie stocks/crypto for starters
-  // nesting the scss
-  // naming divs usefully...
-// other notes:
-  // put the connection to DB within the models. nesting not ideal. recommend thinknig about RDBMS
-
 
 class App extends Component {
   constructor(props) {
@@ -46,11 +25,29 @@ class App extends Component {
       conversionRate: 2, //test rate, will be overwritten by state change
       converted: '',
       history: '', //your amount * conversionrate
-      basecurr: 'USD'
+      basecurr: 'USD',
+      portfolio: [],
+      allRates: {}
     };
     this.curr1Change = this.curr1Change.bind(this);
     this.curr2Change = this.curr2Change.bind(this);
     this.valueChange = this.valueChange.bind(this);
+    this.getPortfolio = this.getPortfolio.bind(this);
+    this.baseCurrChange = this.baseCurrChange.bind(this);
+  }
+
+  getPortfolio() {
+    axios('/user/getPort')
+      .then(res => {
+        console.log('getportfolio axios is happening now')
+        this.setState({portfolio: res}) //spread state in???
+      })
+      .then(()=> {
+        console.log('this is state', this.state)
+      })
+      .catch(err => {
+        console.log('axios caught in getPortfolio on App.jsx', err)
+      })
   }
 
   componentDidMount() {
@@ -81,6 +78,9 @@ class App extends Component {
     this.setState({ curr2: event.target.value });
   }
 
+  baseCurrChange(event) {
+   this.setState({ basecurr: event.target.value });
+  }
   valueChange(event) {
     
     const x = Promise.resolve(this.setState({ value: event.target.value }));
@@ -140,12 +140,15 @@ class App extends Component {
                 <div id="together">
                   <ConversionBox info={this.state} />
                   <ChoiceBox
+                    getPortfolio={this.getPortfolio}
                     info={this.state}
                     curr1Change={this.curr1Change}
                     curr2Change={this.curr2Change}
                     valueChange={this.valueChange}
+                    baseCurrChange={this.baseCurrChange}
                   />
-                  <PositionsTable/>
+                  <PositionsTable info={this.state}
+                   getPortfolio={this.getPortfolio} />
                   </div>
                 <Graph info={this.state} />
               </Route>
--- server/controllers/currencyController.js
+++ server/controllers/currencyController.js
@@ -20,18 +20,26 @@ currencyController.setCookie = async (req, res, next) => {
     // if don't exist, add user
     // if exist, update access token
     // place in db
+    // await pool.query('SELECT * FROM currency_descriptions', (err, data) => {
+    //   // if (err) return next(err);
+    //   console.log('data on line 31', data);
+    //   console.log('err', err);
+    //   return next();
+    // })
+    // change fav_rates to string type 
     await pool.query(`INSERT INTO user_table(user_id, amt_usd, fav_rates, base_currency, token)
-      VALUES('${id}', 0, 'ARRAY ${[]}', 1, '${token}') 
+      VALUES('${id}', 0, ARRAY[1], 1, '${token}') 
       ON CONFLICT (user_id) DO UPDATE SET token = '${token}'`)
       .catch(err => {
-        console.log('err in setCookie', err)
+        console.log('err in setCookie psql query', err)
       })
     // send cookie back to user
     res.cookie('token', token, { maxAge: 1800000});
     // set cookie for id as well
-    res.cookie('idCookie', id);
+    res.cookie('idCookie', id); //goddamnit brian !
+    return next();
   } catch (err) {
-    return next('Error found in setCookie', err)
+    return next('try catch Error found in setCookie', err)
   }
 }
 
@@ -67,24 +75,32 @@ currencyController.checkCookie = async (req, res, next) => {
 // from currency acronynm/ base currency get currency ID
 currencyController.getCurrencyId = async (req, res, next) => {
   try {
-    pool.query(`SELECT * FROM currency_description WHERE currency_acronym = ${req.body.base_currency}`, (err, data) => {
+    pool.query(`SELECT * FROM currency_descriptions WHERE currency_acronym = '${req.body.currency_acronym}'`, (err, data) => {
       if(err) {
-        console.log('Error in getCurrencyId');
+        console.log('Error in getCurrencyId')
+        console.log('this is the error in getCurrencyId in poolquery', err)
         return next(err);
       }
+      console.log('this is data.rows', data.rows)
       res.locals.currency_id = data.rows[0].currency_id
+      console.log(res.locals.currency_id, 'here is res.locals.currency_id')
       return next();
     })
   } catch (err) {
+    console.log('try catch caught in getCurrencyId')
     return next(err)
   }
 }
 
 currencyController.addPortfolio = async (req, res, next) => {
   try {
-    await pool.query(`INSERT INTO positions(currency_id, local_value, user_id)VALUES($1,$2,$3)`, [res.locals.currency_id, req.body.value, req.cookies.id])
+    console.log('user id in addport', req.cookies.idCookie);
+    
+    await pool.query(`INSERT INTO positions(currency_id, local_value, user_id)VALUES($1,$2,$3)`, [res.locals.currency_id, req.body.amt, req.cookies.idCookie])
+    console.log('pool.query in addport occurred')
     return next()
   } catch (err) {
+    console.log('trycatch in addportfolio invoked, error is:', err)
     return next(err)
   }
 }
@@ -102,7 +118,7 @@ currencyController.getCurrencyId = async (req, res, next) => {
 */
 currencyController.getPortfolio = async (req, res, next) => {
   try{ 
-    await pool.query(`SELECT * FROM positions WHERE user_id = '${req.cookies.id}' `, (err, data) =>{
+    await pool.query(`SELECT * FROM positions WHERE user_id = '${req.cookies.idCookie}'`, (err, data) =>{
       res.locals.portfolio = data.rows; 
       return next();
     })
@@ -110,15 +126,24 @@ currencyController.getPortfolio = async (req, res, next) => {
     return next(err)
   }
 } 
+https://api.exchangeratesapi.io/v1/latest?access_key=1ffa62edcf0ab7a273524c03abf11876&base=USD
 
 
-
-
-
-
-
-
-
+currencyController.getAllRates = async (req, res, next) => {
+  try {
+    // taking currency inputs from front end request ML
+    const { curr1, curr2 } = req.body;
+    // request from API the rate at which to convert
+    const result = await fetch(
+      `https://api.exchangeratesapi.io/v1/convert?access_key=${currencyApiKey}&from=${curr1}&to=${curr2}&amount=1`
+    );
+    const json = await result.json();
+    res.locals.rate = json;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
 
 
 
